92308768b7bf3a2f1524bd1180ec8c6464ecc54d
Prince Kushwaha <pkaddon772@gmail.com>
2026-04-21 13:52:10 +0530

    Implement maintenance mode handling in middleware
    Add maintenance mode check in middleware to restrict access.

--- middleware.js
+++ middleware.js
@@ -5,7 +5,21 @@ export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const url = req.nextUrl;
   const pathname = url.pathname;
+  const isMaintenance = process.env.MAINTENANCE_MODE === "true";
 
+  if (isMaintenance) {
+    const allowedEmails = [
+      process.env.ADMIN_EMAIL1,
+      process.env.ADMIN_EMAIL2,
+      process.env.ADMIN_EMAIL3,
+      process.env.ADMIN_EMAIL4,
+      process.env.ADMIN_EMAIL5,
+    ].filter(Boolean); // remove undefined
+
+    if (!token || !allowedEmails.includes(token.email)) {
+      return new NextResponse("🚧 Site under maintenance", { status: 503 });
+    }
+  }
   if (!token) {
     if (pathname.startsWith("/api/protected/")) {
       return NextResponse.json(
